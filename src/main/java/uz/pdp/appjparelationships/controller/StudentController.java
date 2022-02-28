8716cf2ceedb25c033b635bc8117f4e525deb89f
TokhirAsadov <86370041+TokhirAsadov@users.noreply.github.com>
2022-02-28 20:23:48 +0500

    getAllStudents method created successfully

--- src/main/java/uz/pdp/appjparelationships/controller/StudentController.java
+++ src/main/java/uz/pdp/appjparelationships/controller/StudentController.java
@@ -5,10 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
-import uz.pdp.appjparelationships.entity.Student;
+import uz.pdp.appjparelationships.entity.*;
+import uz.pdp.appjparelationships.payload.*;
 import uz.pdp.appjparelationships.repository.StudentRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/student")
@@ -17,38 +19,79 @@ public class StudentController {
     StudentRepository studentRepository;
 
 
-//    @GetMapping("/allStudents")
-//    public List
+    @GetMapping("/allStudents")
+    public List<StudentDto> getAllStudents(){
+        return studentRepository.findAll().stream()
+                .map(this::generateStudentDto)
+                .collect(Collectors.toList());
+    }
 
-    //1. VAZIRLIK
+    private StudentDto generateStudentDto(Student student) {
+        return new StudentDto(
+                student.getId(),
+                student.getFirstName(),
+                student.getLastName(),
+                generateAddressDto(student.getAddress()),
+                generateMyGroupDto(student.getGroup()),
+                student.getSubjects().stream()
+                        .map(this::generateSubjectDto)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    private SubjectDto generateSubjectDto(Subject subject) {
+        return new SubjectDto(
+                subject.getId(),
+                subject.getName()
+        );
+    }
+
+    private MyGroupDto generateMyGroupDto(Group group) {
+        return new MyGroupDto(
+                group.getId(),
+                group.getName(),
+                generateMyFacultyDto(group.getFaculty())
+        );
+    }
+
+    private MyFacultyDto generateMyFacultyDto(Faculty faculty) {
+        return new MyFacultyDto(
+                faculty.getId(),
+                faculty.getName(),
+                generateMyUniversityDto(faculty.getUniversity())
+        );
+    }
+
+    private MyUniversityDto generateMyUniversityDto(University university) {
+        return new MyUniversityDto(
+                university.getId(),
+                university.getName(),
+                generateAddressDto(university.getAddress())
+        );
+    }
+
+    private AddressDto generateAddressDto(Address address) {
+        return new AddressDto(
+                address.getId(),
+                address.getCity(),
+                address.getDistrict(),
+                address.getStreet()
+        );
+    }
+
     @GetMapping("/forMinistry")
     public Page<Student> getStudentListForMinistry(@RequestParam int page) {
-        //1-1=0     2-1=1    3-1=2    4-1=3
-        //select * from student limit 10 offset (0*10)
-        //select * from student limit 10 offset (1*10)
-        //select * from student limit 10 offset (2*10)
-        //select * from student limit 10 offset (3*10)
         Pageable pageable = PageRequest.of(page, 10);
         Page<Student> studentPage = studentRepository.findAll(pageable);
         return studentPage;
     }
 
-    //2. UNIVERSITY
     @GetMapping("/forUniversity/{universityId}")
     public Page<Student> getStudentListForUniversity(@PathVariable Integer universityId,
                                                      @RequestParam int page) {
-        //1-1=0     2-1=1    3-1=2    4-1=3
-        //select * from student limit 10 offset (0*10)
-        //select * from student limit 10 offset (1*10)
-        //select * from student limit 10 offset (2*10)
-        //select * from student limit 10 offset (3*10)
         Pageable pageable = PageRequest.of(page, 10);
         Page<Student> studentPage = studentRepository.findAllByGroup_Faculty_UniversityId(universityId, pageable);
         return studentPage;
     }
-
-    //3. FACULTY DEKANAT
-    //4. GROUP OWNER
-
 
 }
